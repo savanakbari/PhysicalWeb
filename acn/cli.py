96ae14ba805ade5328dce9b6cb7595ad3a297996
Nikhil Joshi <nikhilnvj@gmail.com>
2015-10-25 21:59:36 -0500

    server side completed

--- acn/cli.py
+++ acn/cli.py
@@ -3,16 +3,14 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('localhost', 8888))
 s.send(b'001')
-numbytes = int(bytes.decode(s.recv(1)))
-location = bytes.decode(s.recv(numbytes))
-numbytes = int(bytes.decode(s.recv(2)))
-officehrs = bytes.decode(s.recv(numbytes))
-numbytes = int(bytes.decode(s.recv(2)))
-otherinfo = bytes.decode(s.recv(numbytes))
-numbytes = int(bytes.decode(s.recv(2)))
-desc = bytes.decode(s.recv(numbytes))
-s.close()
-print(location)
-print(officehrs)
-print(otherinfo)
-print(desc)
+numbytes = ''
+while True:
+    b = bytes.decode(s.recv(1))
+    if b != ',':
+        numbytes += b
+    else:
+        break
+numbytes = int(numbytes)
+data = bytes.decode(s.recv(numbytes)).split(',')
+for d in data:
+    print(d)
--- acn/requesthandler.py
+++ acn/requesthandler.py
@@ -7,15 +7,15 @@ def connecttodb():
     return connect(user='root', password='root', host='127.0.0.1', database='physical_web')
 
 
-def handlerequest(clientsocket:socket.socket):
+def handlerequest(clientsocket: socket.socket):
     conn = connecttodb()
     cursor = conn.cursor()
     tagid = bytes.decode(clientsocket.recv(3))
     query = 'select * from tagdata where id={0}'.format(tagid)
     cursor.execute(query)
+    s=[]
     for (id, location, officehours, otherinfo, description) in cursor:
-        s = [str(len(location)), location, str(len(officehours)), officehours, str(len(otherinfo)), otherinfo, str(len(description)),
-             description]
-        s = ''.join(s)
-    clientsocket.send(bytes(s,encoding='utf8'))
+        s = [location + ',', officehours + ',', otherinfo + ',', description]
+        s.insert(0, str(len(''.join(s)))+',')
+    clientsocket.send(bytes(''.join(s),encoding='utf8'))
     clientsocket.close()
